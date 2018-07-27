c6f13c983db56fbe96146880bd116a5a2ef3853d
Tim Lum <twhlum@gmail.com>
2018-07-26 17:16:53 -0700

    Update 08_02_RobotInAGrid.cpp
    Problem description added

--- S09_Ch08_RecursionAndDynamicProgramming/08_02_RobotInAGrid.cpp
+++ S09_Ch08_RecursionAndDynamicProgramming/08_02_RobotInAGrid.cpp
@@ -5,12 +5,15 @@
 // Tim Lum
 // twhlum@gmail.com
 // Created:  2018.07.23
-// Modified: 2018.07.23
+// Modified: 2018.07.26
 //
 
 /*
-#.# - functionName() - P.####
-<Problem Text>
+8.1 - robotInAGrid() - P.135
+Imagine a robot sitting on the upper left corner of grid with 'r' rows and 'c'
+columns. The robot can only move in two directions, right and down, but certain
+cells are "off limits" such that the robot cannot step on them. Design an
+algorithm to find a path for the robot from the top left to the bottom right.
 
 
 
