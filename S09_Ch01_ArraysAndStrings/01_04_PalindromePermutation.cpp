336cbcc952c6b85b0c23d84bf2333ff030504578
Tim Lum <twhlum@gmail.com>
2018-08-21 20:24:45 -0700

    Update 01_04_PalindromePermutation.cpp
    Code confirmed to be functional. TODO: Add whitespace and capitalization parsing.

--- S09_Ch01_ArraysAndStrings/01_04_PalindromePermutation.cpp
+++ S09_Ch01_ArraysAndStrings/01_04_PalindromePermutation.cpp
@@ -8,9 +8,6 @@
 // Modified: 2018.08.21
 //
 
-// TODO: Investigate use of a boolean isEven/isOdd toggle. Investigate use of
-// oddCount to avoid iterating over the count the alphabet
-
 /*
 1.4 - Palindrome Permutation() - P.91
 Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
@@ -19,14 +16,21 @@ Example:
 Input: Tact Coa
 Output: True (permutations: "taco cat", "atco cta", etc.)
 
-Problem Setup and Assumptions:
-  A string may be represented as an array or linked list of chars.
-    Many languages also support the string as a class
-  A permutation is a rearrangement of characters in a string.
-  A palindrome reads the same forward as backward
-   - Ignoring case
-   - Ignoring whitespace
-  Assume valid chars from a-z and A-Z
+
+
+//-----------------------------------------------------------------------------|
+// PROBLEM SETUP AND ASSUMPTIONS
+//-----------------------------------------------------------------------------|
+
+A string may be represented as an array or linked list of chars.
+  Many languages also support the string as a class
+A permutation is a rearrangement of characters in a string.
+A palindrome reads the same forward as backward
+ - Ignoring case
+ - Ignoring whitespace
+Assume valid chars from a-z and A-Z
+
+
 
 //-----------------------------------------------------------------------------|
 // NAIVE, BRUTE FORCE, TERRIBAD APPROACH
@@ -46,18 +50,19 @@ Naive Approach:
   This process may be eased by first sorting the string, then drawing pairs
   of characters until until a deadlock is reached or a palindrome constructed.
 
-Better Approach:
-  Observe that the palindromic quality is really a description of character counts.
-  Palindromes:
-    - Have an even number of every character
-    - But +may+ have an odd number of a single character
-  Hence, if all the characters are even in count (except possibly one),
-  then the string can be configured as a palindrome.
+
 
 //-----------------------------------------------------------------------------|
 // OPTIMIZATIONS
 //-----------------------------------------------------------------------------|
 
+Observe that the palindromic quality is really a description of character counts.
+Palindromes:
+  - Have an even number of every character
+  - But +may+ have an odd number of a single character
+Hence, if all the characters are even in count (except possibly one),
+then the string can be configured as a palindrome.
+  
 1) As we don't care about the specific number of pairs, the table may merely
   track whether the number of characters encountered thus far is even or odd.
 2) A single counter can tally how many odd characters have been encountered
@@ -74,13 +79,6 @@ O( n ) to iterate over the string
 
 Where 'n' is the length of the input) plus:
 
-O( A ) to iterate over the character counts
-
-Where 'A' is the length of the valid alphabet
-
-Formally:
-
-O( n + A )
 
 
 //-----------------------------------------------------------------------------|
@@ -108,6 +106,8 @@ Whenever a cell toggles from 1 to 0, decrement the odd counter
 At the end of the process, if the odd counter is greater than 1, return false.
 Otherwise return true.
 
+
+
 //-----------------------------------------------------------------------------|
 // CODE (C++)
 //-----------------------------------------------------------------------------|
@@ -125,38 +125,42 @@ Otherwise return true.
 // ------------------------------------|
 // Desc:    Determines whether a string is a permutation of a palindrome
 // Params:  string arg1 - The string to analyze
-// PreCons: None
+// PreCons: Additional whitespaces are omitted from the input string if they
+//          do NOT count towards permutative qualities
+//          Whitespaces are included in the input string if they DO count
+//          towards permutative quality.
+//          Capitalization DOES count
 // PosCons: None
 // RetVal:  bool true - The received string is a palindrome permutation
 //          bool false - The received string is not a palindrome permutation
-bool palindromePermutation(string theString) {
-   // Generate a count table and initialize it to 0
-   int charCounts[256];
-   for (int i = 0 ; i < 256 ; i++) {
-      charCounts[i] = 0;
-   }
-
-   // Iterate over the string, incrementing the charCounts table
-   char currCharIndex;
-   for ( int i = 0 ; i < theString.length() ; i++ ) {
-      currCharIndex = (int)theString.at(i);
-      charCounts[currCharIndex]++;
-   }
-
-   // Iterate over the charCounts table and inventory how many odd character counts were found
-   int oddsEncountered = 0;
+bool palindromePermutation( std::string theString ) {
+   // Generate an even/odd table and initialize it to true (counts of 0 are even)
+   bool isEven[ 256 ];
    for ( int i = 0 ; i < 256 ; i++ ) {
-      if (charCounts[i] % 2 != 0) {
-         oddsEncountered++;
-         // Check if this is or is not the first odd value encountered
-         if (oddsEncountered > 1) {
-            return (false);
-         }
+      isEven[ i ] = true;
+   }
+   int oddCounter = 0;
+   // Iterate over the string, toggling the isEven table
+   char currCharIndex;
+   for ( int i = 0 ; i < theString.length( ) ; i++ ) {
+      currCharIndex = ( int )theString.at( i );
+      isEven[ currCharIndex ] = !isEven[ currCharIndex ];
+      // If we just switched to an even count...
+      if( isEven[ currCharIndex ] == true ) {
+         oddCounter--;
       }
-   } // Closing for - all counts checked
-
-   // Not more than one odd count was encountered, therefore...
-   return (true);
+      else {
+         oddCounter++;
+      }
+   } // Closing for - String has been parsed
+   // If the oddCounter is 0 or 1, this is permutable
+   if( oddCounter <= 1 ) {
+      return( true );
+   }
+   else {
+      // Otherwise it isn't permutable.
+      return ( false );
+   }
 } // Closing palindromePermutation(string)
 
 
@@ -176,17 +180,16 @@ bool palindromePermutation(string theString) {
 // RetVal:  int - The exit code (0 for normal, -1 for error)
 int main( int argc, char* argv[ ] ) {
    std::cout << "Test of palindromePermutation( )" << std::endl;
-   std::string test1 = URLify( "www.google.com", 14 );
-   std::string test2 = URLify( "www.my site.com  ", 15 );
-   std::string test3 = URLify( "www.sneakiness.com   ", 19 ); // Trailing whitespace
-   std::cout << "Test 1 : ( www.google.com expected )" << std::endl;
-   std::cout << "           " << test1 << std::endl;
-   std::cout << "Test 2 : ( www.my%20site.com expected )" << std::endl;
-   std::cout << "           " << test2 << std::endl;
-   std::cout << "Test 3 : ( www.sneakiness.com%20 expected )" << std::endl;
-   std::cout << "           " << test3 << std::endl;
+   bool test1 = palindromePermutation( "thisisapermutationthisisapermutationz" );
+   bool test2 = palindromePermutation( "thisisnotapermutation" );
+   bool test3 = palindromePermutation( "TacocaT" ); // Trailing whitespace
+   std::cout << "Test 1 : ( 1 expected )" << std::endl;
+   std::cout << "           " << test1 << std::endl << std::endl;
+   std::cout << "Test 2 : ( 0 expected )" << std::endl;
+   std::cout << "           " << test2 << std::endl << std::endl;
+   std::cout << "Test 3 : ( 1 expected )" << std::endl;
+   std::cout << "           " << test3 << std::endl << std::endl;
    return( 0 );
 } // Closing main( int, char* )
 
 // End of file 01_04_PalindromePermutation.cpp
-
