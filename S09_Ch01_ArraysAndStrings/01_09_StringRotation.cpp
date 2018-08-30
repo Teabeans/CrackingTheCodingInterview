67e9bc6b984be6cd6787761c039396da859d34fa
Tim Lum <twhlum@gmail.com>
2018-08-30 02:25:18 -0700

    Update 01_09_StringRotation.cpp
    Writeup composed, driver added, code confirmed to be working

--- S09_Ch01_ArraysAndStrings/01_09_StringRotation.cpp
+++ S09_Ch01_ArraysAndStrings/01_09_StringRotation.cpp
@@ -5,113 +5,237 @@
 // Tim Lum
 // twhlum@gmail.com
 // Created:  2018.08.28
-// Modified: 2018.07.15
+// Modified: 2018.08.29
 //
 
-NOTE: PLACEHOLDER DOCUMENT! TODO: Complete problem
-
 /*
-1.2 - CheckPermutation() - P.90
-Given two strings, write a method to decide if one is a permutation of the other.
+1.9 - StringRotation() - P.91
+Assume you have a method isSubstring which checks if one word is a substring of
+another. Given two strings s1 and s2, write code to check if s2 is a rotation of
+s1 using only one call to isSubstring.
 
-Problem Setup and Assumptions:
-  A string may be represented as an array or linked list of chars.
-    Many languages also support the string as a class
-  A permutation is a rearrangement of characters in a string.
+e.g. "waterbottle" is a rotation of "erbottlewat"
 
-Naive Approach:
-  Compare all permutations of one string against the other for equivalence.
-  The time complexity may be represented by the number of comparisons required based on N, the length of the strings.
-  As a string of length N will in the worst case require a quantity of comparisons matching the number of permutations available, the pattern will be as follows:
-    (1) char  == 1 combination  (a)
-    (2) chars == 2 combinations (ab, ba)
-    (3) chars == 6 combinations (abc, acb, bac, bca, cab, cba)
-  More broadly, we may say that the number of combinations is factorial in nature:
-  O(N!)
-  1 * 2 * 3 ... * n
-  However, this only represents the number of string comparisons to perform. As such comparisons are char-by-char comparisons down the string, we may expect a time complexity closer to:
-  O(N! * N)
-  Note that this ignores issues of repeated characters or discussions of alphabet size.
 
-Optimizations:
-  1) If the string size is an O(1) accessible field, it may be checked first. Any strings with differing lengths may bypass comparisons; they cannot be permutations of each other.
-  2) Sorting the two strings (an O(NlogN) operation) may allow us to traverse the sorted strings char-by-char looking for discrepancies.
-    - To be permutations, the quantity of all characters must be the same
-    - So if a mismatch is found in the sorted sequence, the quantities differ and the strings cannot be permutations of each other
-  3) However, since we only require a count of the characters involved, we can avoid sorting the strings and merely take a tally
-    - 2 tables will be required representing the frequency of characters within each string
-    - After traversing the string to populate these frequency tables, we can compare the tables for discrepancies
-    - The time complexity may be represented as:
-      - O(N) to traverse the first string
-      - O(N) to traverse the second string
-      - O(A) to traverse the frequency table (where A represent the length of the alphabet)
-    - This has the additional advantage that the strings need not be modified or copied
-  4) A single frequency table may be used by counting up for the first string, then down for the second. This yields a couple advantages:
-    - A slight improvement to memory requirements of the frequency table, as only one alphabet need be stored.
-    - The algorithm may shortcut its comparisons if the decrement value ever drops below 0 (representing more of that character in the second string than the first)
-    - The time complexity may be represented as:
-      - O(N) to traverse the first string
-      - O(N) to traverse the second string
-      - Which simplifies to an O(N) time complexity for this solution
 
-Pseudo Logic:
-  Compare string lengths for equality
-  Declare alphabet table charCounts
-  For each character in string1
-    Add 1 to the appropriate table in charCounts
-  For each character in string2
-    Subtract 1 from the appropriate table in charCounts
-    If the result is <0
-      Return false
+//-----------------------------------------------------------------------------|
+// PROBLEM SETUP AND ASSUMPTIONS
+//-----------------------------------------------------------------------------|
 
-Code (C++):
+A string may be represented as an array or linked list of chars.
+  Many languages also support the string as a class
+A 'rotation' is achieved by moving the first character to the end position or
+vice versus an unspecified number of times.
+Two matching strings count as a rotation (ie. a "full" rotation)
+Rotations do not contain a direction. For instance:
+  WATERBOTTLE => BOTTLEWATER may represent
+    Either (5) letters moved from front to back
+    OR (6) letters moved from back to front
+
+
+
+//-----------------------------------------------------------------------------|
+// NAIVE APPROACH
+//-----------------------------------------------------------------------------|
+
+Treat every character in String1 as a starting point, then iterate across all
+characters in String1 to see if a match can be found against String2.
+
+D E F A B C <- Compare against this
+A B C D E F
+^ - Start here
+
+DEFABC vs ABCDEF (No match!)
+
+A B C D E F
+  ^ - Start here
+
+DEFABC vs BCDEFA (No match!)
+
+A B C D E F
+    ^ - Start here
+
+DEFABC vs CDEFAB (No match!)
+
+A B C D E F
+    ^ - Start here
+
+DEFABC vs DEFABC (Match!)
+
+This solution operates, in the worst case, with a time complexity of:
+
+O( n^2 )
+
+Where 'n' reflects the number of characters in the strings.
+
+
+
+//-----------------------------------------------------------------------------|
+// OPTIMIZATIONS
+//-----------------------------------------------------------------------------|
+
+Observing first that two strings of differing lengths cannot be rotations of one
+another, any two strings of differing lengths need not perform a comparison. They
+may merely return (false).
+
+Additionally, we may observe that doubling a rotated string may yield the original
+unrotated string as a subset.
+
+BOTTLEWATER, for instance, when doubled, becomes BOTTLE WATERBOTTLE WATER (spaces
+added for clarity).
+
+If we are doubling String1, then it is now possible to pass the comparison string
+String2 to isSubstring(). If isSubstring() returns true, then S1 and S2 share a
+rotational relationship with one another.
+
+An additional memory savings might be realized by repeatedly "rotating" String1 in
+the following fashion so as to avoid rewriting the string to twice its original
+size:
+
+Using the hypothetical (true) case below:
+
+String1 == A B C A B C A B C D
+String2 == A B C D A B C A B C
+
+We begin by looking for a common feature (in this case, the first char of String2)
+
+String1 == A B C A B C A B C D
+           ^ Found a feature!
+String2 == A B C D A B C A B C
+           ^ Trying to match this
+
+If no such common feature is found in 'N' observations, then no rotational
+relationship exists
+
+We may then attempt to iterate across both strings seeking a match of length N
+
+String1 == A B C A B C A B C D
+           ^ ^ ^ !
+String2 == A B C D A B C A B C
+           ^ ^ ^ !
+
+If a mismatch occurs, we shift the sought-after feature of String2 and again look
+for a matching feature in String1
+
+String1 == A B C A B C A B C D
+                 ^ ^ ^ ^ ^ ^ ^ Found a feature!
+String2 == A B C D A B C A B C
+                 ^ Reset to here
+
+This process continues until 'N' uninterrupted matches are found between String1
+and String2
+OR
+String2 exhausts its supply of features
+
+String1 == A B C A B C A B C D
+                       ^ Found a feature!
+String2 == A B C D A B C A B C
+           ^ Reset to here
+
+
+
+//-----------------------------------------------------------------------------|
+// TIME COMPLEXITY
+//-----------------------------------------------------------------------------|
+
+Either approach (doubling or iterative comparisons) shall complete in:
+
+O( n )
+
+Where 'n' is the number of characters in the string
+
+
+
+//-----------------------------------------------------------------------------|
+// PSEUDOLOGIC
+//-----------------------------------------------------------------------------|
+
+Compare string lengths for equality
+Double String1
+Call isSubstring on doubled string and String2
+Return result
+
+
+
+//-----------------------------------------------------------------------------|
+// CODE (C++)
+//-----------------------------------------------------------------------------|
 */
+// Compile with:
+// $ g++ --std=c++11 01_09_StringRotation.cpp -o StringRotation
+// Run with:
+// $ ./StringRotation
 
+#include <iostream>
 #include <string>
 
+// NOTE: string.find(string) performs a similar function to isSubstring()
+// It returns -1 on failure, but because its return type is unsigned, this value
+// shall be the maximum integer value possible unless assigned to a signed int.
+
 // (+) --------------------------------|
-// #checkPermutations(string)
+// #stringRotation( string )
 // ------------------------------------|
-// Desc:    Determines whether two strings are permutations of one another or not
+// Desc:    Determines whether two strings are rotations of one another or not
 // Params:  string arg1 - The first string to compare
 //          string arg2 - The second string to compare
 // PreCons: None
 // PosCons: None
-// RetVal:  bool true - The received strings are permutations of each other
-//          bool false - The received strings are not permutations of each other
-bool checkPermutations(string string1, string string2) {
+// RetVal:  bool true - The received strings are rotations of each other
+//          bool false - The received strings are not rotations of each other
+bool stringRotation( std::string string1, std::string string2) {
    // Compare string lengths for equality
-   if ( string1.length( ) != string2.length( ) {
+   if ( string1.length( ) != string2.length( ) ) {
       return( false );
    }
 
-   // Declare and initialize alphabet table charCounts
-   int charCounts[256];
-   for ( int i = 0 ; i < 256 ; i++ ) {
-      charCounts[i] = 0;
+   std::string dblString = string1 + string1;
+   // The search (isSubstring( ))
+   int retVal = dblString.find( string2 );
+   // If string2 was not found within the doubled string...
+   if( retVal == -1 ) {
+      return( false );
+   }
+   // Otherwise...
+   else {
+      return( true );
    }
 
-   // For each character in string1
-   char currChar;
-   for ( int i = 0 ; i < string1.length( ) ; i++ ) {
-      // Add 1 to the appropriate table in charCounts
-      currChar = string1.at( i );
-      charCounts[ (int)currChar ] += 1;
-   } // Closing for - Frequency table loaded
+} // Closing stringRotation()
 
-   // For each character in string2
-   for ( int i = 0 ; i < string2.length( ) ; i++ ) {
-      // Subtract 1 from the appropriate table in charCounts
-      currChar = string2.at( i );
-      charCounts[ (int)currChar ] -= 1;
 
-      // If the result is <0
-      if ( charCounts[ (int)currChar ] < 0 ) {
-         // Return false
-         return( false );
-      }
-   } // Closing for - All comparisons performed
 
-   // No discrepancies found in the two strings, so...
-   return( true );
-}
+//-----------------------------------------------------------------------------|
+// DRIVER
+//-----------------------------------------------------------------------------|
+
+// (+) --------------------------------|
+// #main( int, char* )
+// ------------------------------------|
+// Desc:    Code driver
+// Params:  int arg1 - The number of command line arguments passed in
+//          char* arg2 - The content of the command line arguments
+// PreCons: None
+// PosCons: None
+// RetVal:  int - The exit code (0 for normal, -1 for error)
+int main( int argc, char* argv[ ] ) {
+   std::cout << "Test of stringRotation( )" << std::endl << std::endl;
+
+   std::string tuple1_0 = "WATERBOTTLE";
+   std::string tuple1_1 = "BOTTLEWATER";
+
+   std::cout << "Tuple1_0 vs Tuple1_1 (1 expected):" << std::endl;
+   bool judgement = stringRotation( tuple1_0, tuple1_1 );
+   std::cout << judgement << std::endl << std::endl;
+
+   std::string tuple2_0 = "AGAMEMNON";
+   std::string tuple2_1 = "HERCULES!";
+
+   std::cout << "Tuple2_0 vs Tuple2_1 (0 expected):" << std::endl;
+   judgement = stringRotation( tuple2_0, tuple2_1 );
+   std::cout << judgement << std::endl << std::endl;
+
+   return( 0 );
+} // Closing main( int, char* )
+
+// End of file 01_09_stringRotation.cpp
