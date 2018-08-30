27dedc4f626e143c3805a99c731828ce5b7508bd
Tim Lum <twhlum@gmail.com>
2018-08-30 15:16:50 -0700

    Create 00_00_Template.cpp

---
+++ 00_00_Template.cpp
@@ -0,0 +1,132 @@
+//-----------------------------------------------------------------------------|
+// AUTHORSHIP
+//-----------------------------------------------------------------------------|
+//
+// Tim Lum
+// twhlum@gmail.com
+// Created:  2018.07.23
+// Modified: 2018.08.30
+//
+
+/*
+#.# - functionName() - P.####
+<Problem Text>
+
+
+
+//-----------------------------------------------------------------------------|
+// PROBLEM SETUP AND ASSUMPTIONS
+//-----------------------------------------------------------------------------|
+
+- Setup descriptor 1
+- Setup descriptor 2
+- ...
+
+
+
+//-----------------------------------------------------------------------------|
+// OBSERVATIONS AND INFERENCES
+//-----------------------------------------------------------------------------|
+
+Ipsum lorem
+
+//-----------------------------------------------------------------------------|
+// NAIVE, BRUTE FORCE, TERRIBAD APPROACH
+//-----------------------------------------------------------------------------|
+
+Step1
+   Substep1
+   Substep2
+   ...
+Step2
+   Substep1
+   Substep2
+   ...
+...
+
+
+
+//-----------------------------------------------------------------------------|
+// OPTIMIZATIONS
+//-----------------------------------------------------------------------------|
+
+1) Optimization 1
+   - Detail 1
+   - Detail 2
+   - ...
+2) Optimization 2
+   - Detail 1
+   - Detail 2
+   - ...
+3) ...
+
+
+
+//-----------------------------------------------------------------------------|
+// TIME COMPLEXITY
+//-----------------------------------------------------------------------------|
+
+<Discussion of time complexity>
+
+
+
+//-----------------------------------------------------------------------------|
+// PSEUDOLOGIC
+//-----------------------------------------------------------------------------|
+
+Step1
+   Substep1
+   Substep2
+   ...
+Step2
+   Substep1
+   Substep2
+   ...
+
+
+
+//-----------------------------------------------------------------------------|
+// CODE (C++)
+//-----------------------------------------------------------------------------|
+*/
+
+#include <02_00_LinkedList.cpp>
+#include <iostream>
+#include <string>
+#include <stringstream>
+#include <iomanip>
+#include <random>
+
+// (+) --------------------------------|
+// #functionName(arg1)
+// ------------------------------------|
+// Desc:    <Function Description>
+// Params:  <T> arg1 - <Argument Description>
+// PreCons: NULL
+// PosCons: NULL
+// RetVal:  <Retval Description>
+<RET> <functionName>(<T> arg1) {
+
+} // Closing <functionName>()
+
+
+
+//-----------------------------------------------------------------------------|
+// DRIVER
+//-----------------------------------------------------------------------------|
+
+// (+) --------------------------------|
+// #main( int, char* )
+// ------------------------------------|
+// Desc:    Code driver
+// Params:  int arg1 - The number of command line arguments passed in
+//          char* arg2 - The content of the command line arguments
+// PreCons: None
+// PosCons: None
+// RetVal:  int - The exit code (0 for normal, -1 for error)
+int main( int argc, char* argv[ ] ) {
+   std::cout << "Hello, World!" << std::endl;
+   return( 0 );
+} // Closing main( int, char* )
+
+// End of file <filename>.cpp
